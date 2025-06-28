3dc342206edda202d29b6e39415f96746501108a
safak <safaksahin022@gmail.com>
2025-06-28 16:08:22 +0300

    accesslog service & manager

---
+++ PersonaKey.BusinessLayer/Abstract/IAccessLogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Abstract
+{
+    public interface IAccessLogService
+    {
+        Task<List<AccessLog>> GetAllAsync();
+        Task<AccessLog> GetByIdAsync(int id);
+        Task AddAsync(AccessLog accessLog);
+        Task UpdateAsync(AccessLog accessLog);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ PersonaKey.BusinessLayer/Concrete/AccessLogManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.BusinessLayer.Abstract;
+using PersonaKey.DataAccessLayer.UnitOfWorks.Abstract;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Concrete
+{
+    public class AccessLogManager : IAccessLogService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AccessLogManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddAsync(AccessLog accessLog)
+        {
+            await _unitOfWork.AccessLogs.AddAsync(accessLog);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var log = await _unitOfWork.AccessLogs.GetByIdAsync(id);
+            if (log != null)
+            {
+                await _unitOfWork.AccessLogs.DeleteAsync(log);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+
+        public async Task<List<AccessLog>> GetAllAsync()
+        {
+            return await _unitOfWork.AccessLogs.GetAllAsync();
+        }
+
+        public async Task<AccessLog> GetByIdAsync(int id)
+        {
+            return await _unitOfWork.AccessLogs.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(AccessLog accessLog)
+        {
+            await _unitOfWork.AccessLogs.UpdateAsync(accessLog);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
