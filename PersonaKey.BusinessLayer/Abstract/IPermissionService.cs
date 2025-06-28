e6aa40465463af7f543673a81663d2973cf8e213
safak <safaksahin022@gmail.com>
2025-06-28 16:43:55 +0300

    permission service & manager

---
+++ PersonaKey.BusinessLayer/Abstract/IPermissionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Abstract
+{
+    public interface IPermissionService
+    {
+        Task<List<Permission>> GetAllAsync();
+        Task<Permission> GetByIdAsync(int id);
+        Task AddAsync(Permission permission);
+        Task UpdateAsync(Permission permission);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ PersonaKey.BusinessLayer/Concrete/PermissionManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.BusinessLayer.Abstract;
+using PersonaKey.DataAccessLayer.UnitOfWorks.Abstract;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Concrete
+{
+    public class PermissionManager : IPermissionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PermissionManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddAsync(Permission permission)
+        {
+            await _unitOfWork.Permissions.AddAsync(permission);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var permission = await _unitOfWork.Permissions.GetByIdAsync(id);
+            if (permission != null)
+            {
+                await _unitOfWork.Permissions.DeleteAsync(permission);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+
+        public async Task<List<Permission>> GetAllAsync()
+        {
+            return await _unitOfWork.Permissions.GetAllAsync();
+        }
+
+        public async Task<Permission> GetByIdAsync(int id)
+        {
+            return await _unitOfWork.Permissions.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Permission permission)
+        {
+            await _unitOfWork.Permissions.UpdateAsync(permission);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
