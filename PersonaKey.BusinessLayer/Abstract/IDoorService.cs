0c0fd0b3212f9c995a0e3479aaafeda41bf1ce77
safak <safaksahin022@gmail.com>
2025-06-28 16:37:06 +0300

    door service & manager

---
+++ PersonaKey.BusinessLayer/Abstract/IDoorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Abstract
+{
+    public interface IDoorService
+    {
+        Task<List<Door>> GetAllAsync();
+        Task<Door> GetByIdAsync(int id);
+        Task AddAsync(Door door);
+        Task UpdateAsync(Door door);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ PersonaKey.BusinessLayer/Concrete/DoorManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.BusinessLayer.Abstract;
+using PersonaKey.DataAccessLayer.UnitOfWorks.Abstract;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Concrete
+{
+    public class DoorManager : IDoorService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DoorManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task AddAsync(Door door)
+        {
+            await _unitOfWork.Doors.AddAsync(door);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var door = await _unitOfWork.Doors.GetByIdAsync(id);
+            if (door != null)
+            {
+                await _unitOfWork.Doors.DeleteAsync(door);
+                await _unitOfWork.SaveAsync();
+            }
+        }
+
+        public async Task<List<Door>> GetAllAsync()
+        {
+            return await _unitOfWork.Doors.GetAllAsync();
+        }
+
+        public async Task<Door> GetByIdAsync(int id)
+        {
+            return await _unitOfWork.Doors.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Door door)
+        {
+            await _unitOfWork.Doors.UpdateAsync(door);
+            await _unitOfWork.SaveAsync();
+        }
+    }
+}
