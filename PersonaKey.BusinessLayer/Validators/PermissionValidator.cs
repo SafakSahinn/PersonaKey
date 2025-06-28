bcc23505662e497dd39b8598849bc78eb92565ee
safak <safaksahin022@gmail.com>
2025-06-28 20:09:39 +0300

    permission validator

---
+++ PersonaKey.BusinessLayer/Validators/PermissionValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.BusinessLayer.Validators
+{
+    public class PermissionValidator : AbstractValidator<Permission>
+    {
+        public PermissionValidator()
+        {
+            RuleFor(p => p.RoleId)
+        .GreaterThan(0); // No message will be displayed to the user
+
+            RuleFor(p => p.DoorId)
+        .GreaterThan(0); // No message will be displayed to the user
+        }
+    }
+}
