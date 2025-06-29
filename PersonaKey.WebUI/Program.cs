c9312ce3fda08804170f9a78217c28f53a496d56
safak <safaksahin022@gmail.com>
2025-06-29 17:42:31 +0300

    departman controller

--- PersonaKey.WebUI/Program.cs
+++ PersonaKey.WebUI/Program.cs
@@ -6,15 +6,19 @@ using PersonaKey.DataAccessLayer.Repository.Abstract;
 using PersonaKey.DataAccessLayer.Repository.Concrete;
 using PersonaKey.DataAccessLayer.UnitOfWorks.Abstract;
 using PersonaKey.DataAccessLayer.UnitOfWorks.Concrete;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using PersonaKey.BusinessLayer.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<PersonaKeyContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PersonaKeyConnection")));
 
+// Dependency injection
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -22,14 +26,20 @@ builder.Services.AddScoped<IPersonService, PersonManager>();
 builder.Services.AddScoped<IDepartmentService, DepartmentManager>();
 builder.Services.AddScoped<IRoleService, RoleManager>();
 
+// FluentValidation settings
+builder.Services.AddValidatorsFromAssemblyContaining<DepartmentValidator>();
+builder.Services.AddFluentValidationAutoValidation(options =>
+{
+    options.DisableDataAnnotationsValidation = true;
+});
+builder.Services.AddFluentValidationClientsideAdapters();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
