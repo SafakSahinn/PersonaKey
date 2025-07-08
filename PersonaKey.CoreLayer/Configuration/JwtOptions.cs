d2e787e6475609844c25cc4976f63a694f853159
safak <safaksahin022@gmail.com>
2025-07-08 21:52:45 +0300

    json service add

---
+++ PersonaKey.CoreLayer/Configuration/JwtOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaKey.CoreLayer.Configuration
+{
+    public class JwtOptions
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpireDays { get; set; }
+    }
+}
--- PersonaKey.WebUI/Program.cs
+++ PersonaKey.WebUI/Program.cs
@@ -1,24 +1,65 @@
+using System.Text;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PersonaKey.BusinessLayer.Abstract;
 using PersonaKey.BusinessLayer.Concrete;
+using PersonaKey.BusinessLayer.Validators;
+using PersonaKey.CoreLayer.Configuration;
 using PersonaKey.DataAccessLayer.Context;
 using PersonaKey.DataAccessLayer.Repository.Abstract;
 using PersonaKey.DataAccessLayer.Repository.Concrete;
 using PersonaKey.DataAccessLayer.UnitOfWorks.Abstract;
 using PersonaKey.DataAccessLayer.UnitOfWorks.Concrete;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using PersonaKey.BusinessLayer.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// --------------------------------------------------
+// Configuration: JWT options from appsettings.json
+// --------------------------------------------------
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
+var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtOptions>();
+
+// --------------------------------------------------
+// Authentication & Authorization (JWT)
+// --------------------------------------------------
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+// --------------------------------------------------
+// MVC & Controllers
+// --------------------------------------------------
 builder.Services.AddControllersWithViews();
 
+// --------------------------------------------------
+// DbContext (SQL Server Connection)
+// --------------------------------------------------
 builder.Services.AddDbContext<PersonaKeyContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PersonaKeyConnection")));
 
-// Dependency injection
+// --------------------------------------------------
+// Dependency Injection
+// --------------------------------------------------
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -30,8 +71,9 @@ builder.Services.AddScoped<IDoorService, DoorManager>();
 builder.Services.AddScoped<ICardService, CardManager>();
 builder.Services.AddScoped<IAccessLogService, AccessLogManager>();
 
-
-// FluentValidation settings
+// --------------------------------------------------
+// FluentValidation
+// --------------------------------------------------
 builder.Services.AddValidatorsFromAssemblyContaining<DepartmentValidator>();
 builder.Services.AddFluentValidationAutoValidation(options =>
 {
@@ -41,7 +83,9 @@ builder.Services.AddFluentValidationClientsideAdapters();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// --------------------------------------------------
+// Middleware pipeline
+// --------------------------------------------------
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -53,7 +97,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication(); // JWT ile doðrulama
+app.UseAuthorization();  // Yetkilendirme
 
 app.MapControllerRoute(
     name: "default",
