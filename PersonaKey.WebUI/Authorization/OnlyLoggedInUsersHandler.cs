86e2d7f807b877a3272fdb2132b75d4ac937fd0b
safak <safaksahin022@gmail.com>
2025-07-11 22:49:08 +0300

    authorazition

---
+++ PersonaKey.WebUI/Authorization/OnlyLoggedInUsersHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PersonaKey.BusinessLayer.Abstract;
+
+namespace PersonaKey.WebUI.Authorization
+{
+    public class OnlyLoggedInUsersHandler : AuthorizationHandler<OnlyLoggedInUsersRequirement>
+    {
+        private readonly IAppUserService _appUserService;
+
+        public OnlyLoggedInUsersHandler(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OnlyLoggedInUsersRequirement requirement)
+        {
+            var username = context.User.Identity?.Name;
+
+            if (string.IsNullOrEmpty(username))
+            {
+                return;
+            }
+
+            var user = await _appUserService.GetByUsernameAsync(username);
+
+            if (user?.Role?.RoleAccess?.CanLogin == true)
+            {
+                context.Succeed(requirement); // Authorized
+            }
+        }
+    }
+}
---
+++ PersonaKey.WebUI/Authorization/OnlyLoggedInUsersRequirement.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PersonaKey.WebUI.Authorization
+{
+    public class OnlyLoggedInUsersRequirement : IAuthorizationRequirement
+    {
+        // You can add additional requirements if needed
+    }
+}
