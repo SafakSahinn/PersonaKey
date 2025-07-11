690fec2abc2f9a578ecdffbf97fced79e400f2de
safak <safaksahin022@gmail.com>
2025-07-11 22:06:42 +0300

    AppUserRepository

---
+++ PersonaKey.DataAccessLayer/Repository/Abstract/IAppUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.DataAccessLayer.Repository.Abstract
+{
+    public interface IAppUserRepository : IGenericRepository<AppUser>
+    {
+        Task<AppUser?> GetByUsernameWithRoleAccessAsync(string username);
+    }
+}
---
+++ PersonaKey.DataAccessLayer/Repository/Concrete/AppUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersonaKey.DataAccessLayer.Context;
+using PersonaKey.DataAccessLayer.Repository.Abstract;
+using PersonaKey.EntityLayer.Concrete;
+
+namespace PersonaKey.DataAccessLayer.Repository.Concrete
+{
+    public class AppUserRepository : GenericRepository<AppUser>, IAppUserRepository
+    {
+        private readonly PersonaKeyContext _context;
+
+        public AppUserRepository(PersonaKeyContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser?> GetByUsernameWithRoleAccessAsync(string username)
+        {
+            return await _context.AppUsers
+                .Include(u => u.Role)
+                    .ThenInclude(r => r.RoleAccess)
+                .FirstOrDefaultAsync(u => u.UserName == username);
+        }
+    }
+}
